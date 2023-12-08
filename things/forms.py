fd962a8cdca0bf0e6e6d7addfadedfaf3f55d793
ChrisHerre334 <chrisherre2003@gmail.com>
2023-12-08 03:19:12 +0000

    Third attempt at Exercise 2

--- things/forms.py
+++ things/forms.py
@@ -3,11 +3,15 @@
 # Create your forms here.
 from django import forms
 from .models import Thing
+from django.core.validators import MaxLengthValidator
 
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
 
-    description = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(
+        widget=forms.Textarea,
+        validators=[MaxLengthValidator(limit_value=120)]
+    )
     quantity = forms.IntegerField(widget=forms.NumberInput)
